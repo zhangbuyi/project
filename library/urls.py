4bfbf46ea08caedc5ea27aa33b22c8983a77787f
zhangbuyi <zhangbuyi@qq.com>
2018-10-25 17:04:54 +0800

    二次上传

--- library/urls.py
+++ library/urls.py
@@ -5,4 +5,6 @@ from library import views
 
 urlpatterns = [
     url(r'^login/',views.login_view),
+    url(r'^main/', views.main_view),
+    url(r'^library_modify/', views.library_modify_view),
 ]
--- library/views.py
+++ library/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def login_view(request):
     return render(request,"login.html")
+
+
+def main_view(request):
+    return render(request,"main.html")
+
+
+def library_modify_view(request):
+    return render(request,"library_modify.html")
