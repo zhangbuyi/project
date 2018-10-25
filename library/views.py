826141d8d605584795420dc2c7602cdbf7ed9cd5
zhangbuyi <zhangbuyi@qq.com>
2018-10-25 21:12:53 +0800

    三次上传 完美前端页面

--- library/views.py
+++ library/views.py
@@ -11,3 +11,57 @@ def main_view(request):
 
 def library_modify_view(request):
     return render(request,"library_modify.html")
+
+def manager_view(request):
+    return render(request,"manager.html")
+
+def parameter_modify_view(request):
+    return render(request,"parameter_modify.html")
+
+
+def bookcase_view(request):
+    return render(request,"bookcase.html")
+
+
+def readerType_view(request):
+    return render(request,"readerType.html")
+
+
+def reader_view(request):
+    return render(request,"reader.html")
+
+
+def bookType_view(request):
+    return render(request,"bookType.html")
+
+
+def book_view(request):
+    return render(request,"book.html")
+
+
+def bookBorrow_view(request):
+    return render(request,"bookBorrow.html")
+
+
+def bookRenew_view(request):
+    return render(request,"bookRenew.html")
+
+
+def bookBack_view(request):
+    return render(request,"bookBack.html")
+
+
+def bookQuery_view(request):
+    return render(request,"bookQuery.html")
+
+
+def borrowQuery_view(request):
+    return render(request,"borrowQuery.html")
+
+
+def bremind_view(request):
+    return render(request,"bremind.html")
+
+
+def pwd_Modify_view(request):
+    return render(request,"pwd_Modify.html")
